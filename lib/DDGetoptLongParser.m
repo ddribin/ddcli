c5fb160df6e91e0119ecfdab2119857200f00859
Dave Dribin <dave@dribin.org>
2014-03-04 23:39:42 -0600

    Use consistent integer types

--- lib/DDGetoptLongParser.m
+++ lib/DDGetoptLongParser.m
@@ -184,9 +184,9 @@ dd_getopt_long_only(int nargc, char * const *nargv, const char *options,
 - (NSArray *)parseOptionsWithArguments:(NSArray *)arguments
                                command:(NSString *)command;
 {
-    int argc = [arguments count];
+    NSUInteger argc = [arguments count];
     char ** argv = alloca(sizeof(char *) * ( argc + 1 ) );
-    int i;
+    NSUInteger i;
     for (i = 0; i < argc; i++)
     {
         NSString * argument = [arguments objectAtIndex: i];
